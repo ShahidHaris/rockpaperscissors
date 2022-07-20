14b3ff04dc5abfbac9dedb0faa6a22cd64455272
ShahidHaris <alharithharis@gmail.com>
2022-07-20 07:15:20 +0100

    write a function to prompt the user to enter a choice and then verify the validity of the input

--- script.js
+++ script.js
@@ -39,15 +39,25 @@ function playRound(playerSelection, computerSelection) {
         console.log(`you win, ${a} beats ${b}`);
     } else if (a == 'paper' && b == 'scissors') {
         console.log(`computer win, ${a} beats ${b}`);
-    }else {
+    } else if (a == b){
         console.log(`It's a draw, try again`);
-        playRound(playerSelection, computerSelection)
     }
 }
 
-let selection = "rock";
-const playerSelection = makeCaseInsensitive(selection); 
+function selection () {
+    return prompt(`Select either of the three: 'rock' 'paper' or 'sciccors'`);
+    let state;
+    if (params == 'rock' || params == 'paper' || params == 'scissors') {
+        state = params;
+    }   else {
+
+    }
+} 
+
+let  verify = makeCaseInsensitive(selection()); 
+let playerSelection = verifyEntery(verify)
 const computerSelection = getComputerChoice();
+
 console.log(playRound(playerSelection, computerSelection));
 
 
